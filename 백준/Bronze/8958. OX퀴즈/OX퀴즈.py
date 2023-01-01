796029d1e4f9508a698f6dbcf4297d5e45fa44d6
김예진 <yeajin7933@gmail.com>
2023-01-01 22:52:43 +0900

    [Bronze II] Title: OX퀴즈, Time: 44 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+for i in range (n):
+    a = list(input())
+    score = 0
+    sum = 0
+    for j in (a):
+        if j =="O":
+            score += 1
+        else:
+            score = 0
+        sum = sum + score
+    print(sum)
