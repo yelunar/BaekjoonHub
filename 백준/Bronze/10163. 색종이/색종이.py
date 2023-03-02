2f2a55f878d6855d6687596ae216868ac613d58c
김예진 <yeajin7933@gmail.com>
2023-03-02 14:31:08 +0900

    [Bronze I] Title: 색종이, Time: 2284 ms, Memory: 101792 KB -BaekjoonHub

---
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -0,0 +1,19 @@
+arr = [[''] * 1001 for i in range(1001)] # 도화지
+N = int(input()) # 색종이 장수
+for k in range(N):
+    x, y, width, height = map(int, input().split())
+    for i in range(y, y+height):
+        for j in range(x, x+width):
+            arr[i][j] = str(k)
+result = []
+cnt = 0
+for k in range(N):
+    for i in range(1001):
+        for j in range(1001):
+            if arr[i][j] == str(k):
+                cnt += 1
+    result.append(cnt)
+    cnt = 0
+
+for i in range(len(result)):
+    print(result[i])
