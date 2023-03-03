116dd8e6a0cbe7de69c9cf42ca441c4d30c1b9a5
김예진 <yeajin7933@gmail.com>
2023-03-03 17:52:27 +0900

    Update 소트인사이드.py

--- 백준/Silver/1427. 소트인사이드/소트인사이드.py
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.py
@@ -5,3 +5,16 @@ ans = ''
 for i in range(len(a)):
     ans += str(a[i])
 print(ans)
+
+# 풀이2-------------------------
+import sys
+# sys.stdin = open('input.txt')
+input = sys.stdin.readline
+
+a = list(input().rstrip())
+a.sort(reverse=True)
+
+ans = ''
+for i in range(len(a)):
+    ans += str(a[i])
+print(ans)
