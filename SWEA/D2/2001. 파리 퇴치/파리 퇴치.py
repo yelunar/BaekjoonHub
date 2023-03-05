4a77ea002b6c3ec33cd062c0d11cdd915832ccdb
김예진 <yeajin7933@gmail.com>
2023-03-05 18:04:18 +0900

    [D2] Title: 파리 퇴치, Time: 167 ms, Memory: 62,004 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
@@ -1,28 +1,16 @@
-T = int(input()) # tc
+T = int(input())
 for tc in range(1, T+1):
     N, M = map(int, input().split())
     arr = [list(map(int, input().split())) for _ in range(N)]
-    result = [] # 합 다 넣을 list
-
-    dx = []
-    dy = []
-
-    for i in range(M): # 0 1 2 n=3일때
-        for j in range(M):
-            dx.append(i)
-            dy.append(j)
-
-    cnt = 0
-    for i in range(N-M+1):
-        for j in range(N-M+1):
-            
-            # cnt = arr[i][j]
-            for k in range(M**2):
-                x = j +dx[k]
-                y = i +dy[k]
-                cnt += arr[y][x]
-
-            result.append(cnt)
-            cnt = 0
-
-    print(f'#{tc} {max(result)}')
+    cnt = []
+    for i in range(N):
+        for j in range(N):
+            tmp = 0
+            for k in range(M):
+                for l in range(M):
+                    if 0<= i+k < N and 0<= j+l < N:
+                        tmp += arr[i+k][j+l]
+            cnt.append(tmp)
+    
+    ans = max(cnt)
+    print(f'#{tc} {ans}')
