8a061228115f410f9a0e1ccfd5954c42c3e8d2d8
김예진 <yeajin7933@gmail.com>
2023-03-18 01:21:20 +0900

    [Silver I] Title: 안전 영역, Time: 1476 ms, Memory: 32900 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -1,47 +1,44 @@
 import sys
+sys.setrecursionlimit(100000)
 
-n = int(sys.stdin.readline())
-land = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+# sys.stdin = open('input.txt')
 
-mini = 101
-maxi = 0
-for a in range(n):
-    for b in range(n):
-        if land[a][b] < mini:
-            mini = land[a][b]
-        if land[a][b] > maxi:
-            maxi = land[a][b]
+def dfs(x, y, number):
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
 
-def safe(arr, num):
-    global result
-    temp = 0
-    visited = [[0] * n for _ in range(n)]
-    for i in range(n):
-        for j in range(n):
-            if arr[i][j] > num and visited[i][j] == 0:
-                temp += 1
-                queue = [i, j]
-                visited[i][j] = 1
-                while queue:
-                    x = queue.pop(0)
-                    y = queue.pop(0)
-                    dx, dy = [1, -1, 0, 0], [0, 0, -1, 1]
-                    for k in range(4):
-                        nx = x + dx[k]
-                        ny = y + dy[k]
-                        if 0 <= nx < n and 0 <= ny < n and arr[nx][ny] > num and visited[nx][ny] == 0:
-                            queue.append(nx)
-                            queue.append(ny)
-                            visited[nx][ny] = 1
+        if 0<= nx < n and 0<= ny < n and visited[nx][ny] == 0:
+            if arr[nx][ny] > number:
+                visited[nx][ny] = 1
+                dfs(nx, ny, number)
 
-    if temp > result:
-        result = temp
+n = int(input())
+arr = []
+max_num = 0
+ans = 1
 
-if mini == maxi:
-    print(1)
-else:
-    result = 0
-    for num in range(mini, maxi):
-        safe(land, num)
+dx = [0,0,1,-1]
+dy = [1,-1,0,0]
 
-    print(result)
+for i in range(n):
+    tmp = list(map(int, input().split()))
+    arr.append(tmp)
+
+    for j in tmp:
+        if j > max_num:
+            max_num = j
+
+for i in range(max_num):
+    visited = [[0]*n for _ in range(n)]
+    cnt = 0
+
+    for j in range(n):
+        for k in range(n):
+            if arr[j][k] > i and visited[j][k] == 0:
+                cnt += 1
+                visited[j][k] = 1
+                dfs(j, k, i)
+    
+    ans = max(ans, cnt)
+print(ans)
