87432655535334cdeaea4a3a7f87487830f0b2c9
김예진 <yeajin7933@gmail.com>
2023-03-18 16:45:05 +0900

    [Silver III] Title: N과 M (2), Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,23 +1,18 @@
 import sys
 # sys.stdin = open("input.txt")
 input = sys.stdin.readline
-from itertools import permutations
 
 n, m = map(int, input().split())
-arr = []
-# arr.append(permutations(range(3), 2))
+lst = []
 
+def backtracking(s):
+    if len(lst) == m:
+        print(*lst)
+        return
+    else:
+        for i in range(s, n+1):
+            lst.append(i)
+            backtracking(i+1)
+            lst.pop()
 
-for i in permutations(range(1, n+1), m):
-    a = sorted(set(i))
-    arr.append(a)
-
-ans = []
-
-for i in arr:
-    if i not in ans:
-        ans.append(i)
-
-for i in ans:
-    # k = ''.join(i)
-    print(*i)
+backtracking(1)
