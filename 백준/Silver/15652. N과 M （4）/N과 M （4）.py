e031dfe2b7c7dce57f69427454554ffbeec8f833
김예진 <yeajin7933@gmail.com>
2023-03-18 16:52:48 +0900

    [Silver III] Title: N과 M (4), Time: 56 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,18 @@
+import sys
+# sys.stdin = open("input.txt")
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+lst = []
+
+def backtracking(s):
+    if len(lst) == m:
+        print(*lst)
+        return
+    else:
+        for i in range(s, n+1):
+            lst.append(i)
+            backtracking(i)
+            lst.pop()
+
+backtracking(1)
