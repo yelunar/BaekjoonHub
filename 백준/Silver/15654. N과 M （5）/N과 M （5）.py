5a022086396062334c7a515552073120f66c83cc
김예진 <yeajin7933@gmail.com>
2023-03-20 12:18:03 +0900

    [Silver III] Title: N과 M (5), Time: 220 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -0,0 +1,27 @@
+import sys
+# sys.stdin = open("input.txt")
+input = sys.stdin.readline
+
+# N개의 자연수 중에서 M개를 고른 수열
+
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+arr.sort()
+lst = []
+
+def backtracking(s):
+
+
+    if len(lst) == m:
+        print(*lst)
+        return
+    else:
+        for i in range(n):
+            if arr[i] in lst:
+                pass
+            else:
+                lst.append(arr[i])
+                backtracking(arr[i])
+                lst.pop()
+            
+backtracking(arr[0])
