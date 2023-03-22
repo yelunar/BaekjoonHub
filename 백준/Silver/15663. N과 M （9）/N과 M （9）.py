a08d25dbc7ca2d4d6f95e61110d2444ce5a13901
김예진 <yeajin7933@gmail.com>
2023-03-22 15:16:34 +0900

    [Silver II] Title: N과 M (9), Time: 104 ms, Memory: 34324 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.py
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -1,22 +1,58 @@
-def dfs(n, tlst):
-    if n==M:
-        ans.append(tlst)
-        return
+import sys
+# sys.stdin = open("input.txt")
+input = sys.stdin.readline
 
-    prev = 0
-    for j in range(N):
-        if v[j]==0 and prev!=lst[j]:
-            prev=lst[j]
-            v[j]=1
-            dfs(n+1, tlst+[lst[j]])
-            v[j]=0
+# N개의 자연수 중에서 M개를 고른 수열
+# 이전 값 추가해서 현재넣을값과 같은지 체크해야함
 
-N, M = map(int, input().split())
-lst = sorted(list(map(int, input().split())))
+def dfs(n, lst):
+    if n == m : #길이 같으면 종료
+        ans.append(lst)
+        return 
+    
+    prev = 0 # 이전 값 저장
+    for i in range(N):
+        if visited[i] == 0 and prev != arr[i]:
+            prev = arr[i]
+            visited[i] = 1
+            dfs(n+1, lst+[arr[i]])
+            visited[i] = 0
+
+N, m = map(int, input().split())
+arr = sorted(list(map(int, input().split())))
 
 ans = []
-v = [0]*N
+visited = [0]*N
+
 dfs(0, [])
 
-for lst in ans:
-    print(*lst)
+for i in ans:
+    print(*i)
+
+
+# def backtracking(s):
+    
+
+# # 완전히 틀린 방법
+# n, m = map(int, input().split())
+# arr = list(map(int, input().split()))
+# arr.sort()
+
+# lst = []
+# ans = []
+# def backtracking(s):
+
+#     if len(lst) == m:
+#         # print(*lst)
+#         ans.append(lst)
+#         # print(ans)
+#         return ans
+#     else:
+#         for i in range(n):
+#             lst.append(arr[i]) # 이렇게하면 얕은 복사 일어나서 
+#             backtracking(i)
+#             lst.pop() # 여기서 리스트가 모조리 사라짐..
+
+# backtracking(arr[0])
+
+# print(ans)
