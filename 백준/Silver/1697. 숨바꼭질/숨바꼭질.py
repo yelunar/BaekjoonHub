c6acbfffb95ac55a55cc885a87e4e124ec45ea03
김예진 <yeajin7933@gmail.com>
2023-04-05 01:25:09 +0900

    [Silver I] Title: 숨바꼭질, Time: 104 ms, Memory: 35264 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,20 +1,30 @@
 import sys
-from collections import deque
+# sys.stdin = open('input.txt')
 input = sys.stdin.readline
+sys.setrecursionlimit(10000)
 
-def hideseek(start):
-    q = deque()
-    q.append(start)
-    visited[start] = 1
-    while q:
-        c = q.popleft() #current
-        if c == k:
-            return visited[c] - 1
-        for i in (c-1, c+1, 2*c):
-            if 0 <= i <= 100000 and not visited[i]:
-                visited[i] = visited[c] + 1
-                q.append(i)
+"""
+하.. 변수명 겹치는거 써서 많이 틀림 ;;
+"""
+from collections import deque
 
 n, k = map(int, input().split())
 visited = [0 for _ in range(100001)]
-print(hideseek(n))
+
+def bfs():
+    queue = deque()
+    queue.append(n)
+    visited[n] = 1  # 이거 안해줘서 이지경이 났다.. 
+
+    while queue:
+        s = queue.popleft()
+        if s == k:
+            print(visited[s]-1)
+            break   
+        
+        for ns in (s-1, s+1, s*2):
+            if 0<= ns < 100001 and not visited[ns]:
+                visited[ns] = visited[s] + 1
+                queue.append(ns)
+    
+bfs()
