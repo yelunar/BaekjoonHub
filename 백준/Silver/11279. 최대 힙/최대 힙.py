9d069e5ed76944016fe0c995f8c3e3886d678ae9
Yejin Kim <yeajin7933@gmail.com>
2023-04-12 21:20:44 +0900

    [Silver II] Title: 최대 힙, Time: 132 ms, Memory: 37180 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.py
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -1,16 +1,16 @@
 import sys
-import heapq
+input = sys.stdin.readline
+
+from heapq import heappop, heappush
 
 heap = []
-
-N = int(input()) # 연산 개수
-
-for _ in range(N):
-    num = int(sys.stdin.readline())
-    if num != 0:
-        heapq.heappush(heap, -num)
-    else:
-        try:
-            print(-1*heapq.heappop(heap))
-        except:
+n = int(input()) #연산개수
+for _ in range(n):
+    num = int(input())
+    if num == 0:
+        if len(heap)>0:  # 힙이 비어있는 경우에는 heappop을 수행하지 않도록 예외 처리
+            print(-1*heappop(heap))
+        else:
             print(0)
+    else:
+        heappush(heap, -num)
