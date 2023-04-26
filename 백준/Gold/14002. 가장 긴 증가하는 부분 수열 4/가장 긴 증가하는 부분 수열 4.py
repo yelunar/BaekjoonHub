16e830d4d904259491bb6b18bbb76fc48f28173a
Yejin Kim <yeajin7933@gmail.com>
2023-04-26 22:37:01 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 160 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.py
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.py
@@ -1,27 +1,26 @@
 import sys
-# sys.stdin = open('input.txt')
+# sys.stdin = open('input2.txt')
 input = sys.stdin.readline
-sys.setrecursionlimit(10000)
+sys.setrecursionlimit(100000)
 
 """
-수열 A가 주어졌을 때, 가장 긴 증가하는 부분 수열을 구하는 프로그램을 작성
 """
-n = int(input()) # 수열길이
-arr = list(map(int, input().split())) # 10 20 10 30 20 50
-dp = [1] * (n+1)
+n = int(input())
+arr = list(map(int, input().split()))
+dp = [1 for _ in range(n)]
 
-for i in range(len(arr)):
+for i in range(1, n):
     for j in range(i):
-        if arr[j] < arr[i]:
+        if arr[i] > arr[j]:
             dp[i] = max(dp[i], dp[j]+1)
-num = max(dp)
-print(num)
 
-ans = []
+print(max(dp))
+
+x = []
+idx = max(dp)
 for i in range(n-1, -1, -1):
-    if dp[i] == num:
-        ans.append(arr[i])
-        num -= 1
-ans.reverse()
-for i in ans:
-    print(i, end=' ')
+    if dp[i] == idx:
+        x.append(arr[i])
+        idx -= 1
+x.reverse()
+print(*x)
