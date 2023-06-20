8aedf9b711c2b52c4206c27fabdaad15468f4757
Yejin Kim <yeajin7933@gmail.com>
2023-06-20 22:04:23 +0900

    [Gold V] Title: 1학년, Time: 0 ms, Memory: 2036 KB -BaekjoonHub

---
+++ 백준/Gold/5557. 1학년/1학년.cc
@@ -0,0 +1,49 @@
+#include<iostream>
+
+using namespace std;
+
+/*
+ 음수를 배우지 않았고, 20을 넘는 수는 모른다
+ 왼쪽부터 계산할 때, 중간에 나오는 수가 모두 0 이상 20 이하
+ 더하기 빼기 등호만 사용  
+ ans => 올바른 등식의 개수 
+*/
+
+int n; // 숫자의 개수
+int arr[100]; 
+long long dp[100][21];
+
+int main(){
+	//freopen("input.txt","r",stdin);
+	std::ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	cin >> n;
+	for(int i=0; i<n; i++){
+		cin >> arr[i];
+	}
+	
+	int ans = arr[n-1]; // 나와야하는 숫자
+	
+	dp[0][arr[0]] = 1; // 제일 첫번째 숫자는 그냥 들어감
+	
+	for(int i=1; i<n-1; i++){
+		for(int j=0; j<=20; j++){
+			//if(dp[i-1][j] == 0) continue;
+			if(j+arr[i]<=20){
+				dp[i][j+arr[i]] += dp[i-1][j];
+			}
+			if(j-arr[i]>=0){
+				dp[i][j-arr[i]] += dp[i-1][j];
+			}
+			
+		}
+	} 
+	
+	long long cnt;
+	cnt = dp[n-2][ans];
+	cout << cnt <<"\n";
+
+	return 0;
+	
+}
