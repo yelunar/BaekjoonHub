70b9ed149a3141e42c2204046af751d55310aa8c
Yejin Kim <yeajin7933@gmail.com>
2023-06-29 22:15:59 +0900

    [level 2] Title: H-Index, Time: 0.00 ms, Memory: 9.91 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.py
+++ 프로그래머스/lv2/42747. H－Index/H－Index.py
@@ -2,10 +2,11 @@
 # 논문 n편 중 h번 이상 인용된 논문에 h편 이상이고 나머지 논문이 h 번 이하
 
 def solution(citations):
-    
+    books = len(citations)
     citations.sort()
-    for idx, citation in enumerate(citations):
-        if citation >= len(citations) - idx:
-            return len(citations) - idx
+    
+    for i in range(len(citations) - 1):
+        if citations[i] >= books-i:
+            return books-i
         
     return 0
