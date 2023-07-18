457dec492afc834d0b15c1b24193b4f3b308f87f
Yejin Kim <yeajin7933@gmail.com>
2023-07-18 23:49:44 +0900

    [Gold IV] Title: 운동, Time: 2692 ms, Memory: 116612 KB -BaekjoonHub

---
+++ 백준/Gold/1956. 운동/운동.py
@@ -0,0 +1,29 @@
+import sys
+input = sys.stdin.readline
+# sys.stdin = open('input3.txt')
+# sys.setrecursionlimit(10**6)
+
+"""
+최소 사이클의 도로 길이의 합을 출력
+"""
+INF = sys.maxsize
+v, e = map(int, input().split(' '))
+arr = list(list(INF for _ in range(v+1)) for _ in range(v+1))
+for _ in range(e):
+    a, b, c = map(int, input().split(' '))
+    arr[a][b] = c
+
+# 플로이드 워셜
+for k in range(1, v+1):
+    for i in range(1, v+1):
+        for j in range(1, v+1):
+            arr[i][j] = min(arr[i][j], arr[i][k]+arr[k][j])
+
+ans = INF  # 정답을 최대값으로 설정해둔다
+for i in range(1, v+1):
+    ans = min(ans, arr[i][i])
+
+if ans == INF:
+    ans = -1
+
+print(ans)
